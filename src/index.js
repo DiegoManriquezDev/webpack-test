43c6af3dbd1b2517c6b7b0e01c51bec42b0e06cc
Diegodev <diegodev.manriquez@gmail.com>
2022-05-02 12:28:29 -0400

    Updating style.css and Images

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import _ from 'lodash';
-import myName from './myName';
-import {functionOne,functionTwo} from './myModule'
+import './style.css';
+import Icon from './icon.png'
 
 function component() {
     const element = document.createElement('div');
   
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack', 'Diego'], ' ');
-    element.textContent = myName('Diego');
+    element.classList.add('hello');
+
+
+    //Add the image to aour existing div
+    const myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
+
+
     return element;
   }
   
